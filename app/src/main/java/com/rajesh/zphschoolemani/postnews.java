c96f4889bd8a70323c9112cd9b34db9479510e18
rajeshp808 <rajeshp808@gmail.com>
2018-10-24 00:07:52 +0530

    //  postnews.java draft version ready.. no compiler error but runtime //errors a lot

--- app/src/main/java/com/rajesh/zphschoolemani/postnews.java
+++ app/src/main/java/com/rajesh/zphschoolemani/postnews.java
@@ -1,18 +1,30 @@
 package com.rajesh.zphschoolemani;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class postnews extends AppCompatActivity {
     private ImageButton imageBtn; private static final int GALLERY_REQUEST_CODE = 2;
@@ -22,64 +34,84 @@ public class postnews extends AppCompatActivity {
     private DatabaseReference mDatabaseUsers; private FirebaseUser mCurrentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postnews);
+        Toast.makeText(getApplicationContext(), "postnews starts", Toast.LENGTH_LONG).show();
         // initialize layout object
 
-        postBtn = (Button)findViewById(R.id.bt_postbutton);
-        textDesc = (EditText)findViewById(R.id.et_News_Description);
-        textTitle = (EditText)findViewById(R.id.et_NewsTitle);
+        postBtn = (Button) findViewById(R.id.bt_postbutton);
+        textDesc = (EditText) findViewById(R.id.et_News_Description);
+        textTitle = (EditText) findViewById(R.id.et_NewsTitle);
         storage = FirebaseStorage.getInstance().getReference();
-        databaseRef = database.getInstance().getReference().child(“Blogzone”);
+        databaseRef = database.getInstance().getReference().child("News");
         mAuth = FirebaseAuth.getInstance();
         mCurrentUser = mAuth.getCurrentUser();
-        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child(“Users”).child(mCurrentUser.getUid());
-        imageBtn = (ImageButton)findViewById(R.id.imageBtn);
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+        imageBtn = (ImageButton) findViewById(R.id.imageButton);
         //picking image from gallery
         imageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType(“image/*”);
- startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
- } });
- // posting to Firebase
- postBtn.setOnClickListener(new View.OnClickListener() {
- @Override
- public void onClick(View view) {
- Toast.makeText(PostActivity.this, “POSTING…”, Toast.LENGTH_LONG).show();
- final String PostTitle = textTitle.getText().toString().trim();
- final String PostDesc = textDesc.getText().toString().trim();
- // do a check for empty fields
- if (!TextUtils.isEmpty(PostDesc) && !TextUtils.isEmpty(PostTitle)){
- StorageReference filepath = storage.child(“post_images”).child(uri.getLastPathSegment());
- filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
- @Override
- public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
- @SuppressWarnings(“VisibleForTests”)
- //getting the post image download url
- final Uri downloadUrl = taskSnapshot.getDownloadUrl();
- Toast.makeText(getApplicationContext(), “Succesfully Uploaded”, Toast.LENGTH_SHORT).show();
- final DatabaseReference newPost = databaseRef.push();
- //adding post contents to database reference
- mDatabaseUsers.addValueEventListener(new ValueEventListener() {
- @Override
- public void onDataChange(DataSnapshot dataSnapshot) {
- newPost.child(“title”).setValue(PostTitle);
- newPost.child(“desc”).setValue(PostDesc);
- newPost.child(“imageUrl”).setValue(downloadUrl.toString());
- newPost.child(“uid”).setValue(mCurrentUser.getUid());
- newPost.child(“username”).setValue(dataSnapshot.child(“name”).getValue())
- .addOnCompleteListener(new OnCompleteListener<Void>() {
- @Override
- public void onComplete(@NonNull Task<Void> task) {
- if (task.isSuccessful()){
- Intent intent = new Intent(PostActivity.this, MainActivity.class);
- startActivity(intent);
- }}});}
- @Override
- public void onCancelled(DatabaseError databaseError) {
- } }); } }); }}}); }
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+            }
+        });
+        // posting to Firebase
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(postnews.this, "POSTING…", Toast.LENGTH_LONG).show();
+                final String PostTitle = textTitle.getText().toString().trim();
+                final String PostDesc = textDesc.getText().toString().trim();
+                // do a check for empty fields
+                if (!TextUtils.isEmpty(PostDesc) && !TextUtils.isEmpty(PostTitle)) {
+                    StorageReference filepath = storage.child("post_images").child(uri.getLastPathSegment());
+                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests")
+                            //getting the post image download url
+                            final Task<Uri> downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                            Toast.makeText(getApplicationContext(), "Succesfully Uploaded", Toast.LENGTH_SHORT).show();
+                            final DatabaseReference newPost = databaseRef.push();
+                            //adding post contents to database reference
+                            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    newPost.child("title").setValue(PostTitle);
+                                    newPost.child("desc").setValue(PostDesc);
+                                    newPost.child("imageUrl").setValue(downloadUrl.toString());
+                                    newPost.child("uid").setValue(mCurrentUser.getUid());
+                                    newPost.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Intent intent = new Intent(postnews.this, MainActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "Exception at postnews "+ex.getMessage(), Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+
+ }
  @Override
  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
  super.onActivityResult(requestCode, resultCode, data);
@@ -87,7 +119,9 @@ public class postnews extends AppCompatActivity {
  if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
  uri = data.getData();
  imageBtn.setImageURI(uri);
- }}}
-
-    }
+ }
+ }
 }
+
+
+
