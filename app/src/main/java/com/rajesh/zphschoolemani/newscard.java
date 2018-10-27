f4d849d5c736f967b66f2c1f840f6413aee2c91a
rajeshp808 <rajeshp808@gmail.com>
2018-10-27 23:14:35 +0530

    // linear layout added, card layout added , need to add recycler view

---
+++ app/src/main/java/com/rajesh/zphschoolemani/newscard.java
@@ -0,0 +1,13 @@
+package com.rajesh.zphschoolemani;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newscard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newscard);
+    }
+}
